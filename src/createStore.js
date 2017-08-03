7aea97cdae92e674bfef9c9a5edcc648545a2e40
sochetanie <allagorik2901@gmail.com>
2017-08-03 13:16:14 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,23 +1,23 @@
 import { render } from './index.js'
 
 export default function createStore(reducer){
-  let state;
+  let state
 
   function dispatch(action){
-    state = reducer(state, action);
-    console.log(`the action is`);
-    console.log(action);
-    console.log(`the state is`);
-    console.log(state);
-    render();
-  };
+    state = reducer(state, action)
+    console.log(`the action is`)
+    console.log(action)
+    console.log(`the state is`)
+    console.log(state)
+    render()
+  }
 
   function getState(){
-    return state;
-  };
+    return state
+  }
 
   return {
     dispatch,
     getState
-  };
-};
+  }
+}
